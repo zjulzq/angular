e98a1d808c14124784cb6c7adebb0f4b901a0bd9
li_z <li_z@worksap.co.jp>
2014-03-04 18:48:11 +0800

    try

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -2,7 +2,10 @@
 
 /* Controllers */
 
-angular.module('controllers', []).controller('MyCtrl1', [ function() {
+angular.module('controllers', []).controller('signinCtrl',
+		[ '$scope', function($scope) {
+			$scope.data = 'Hello world';
+		} ]).controller('MyCtrl1', [ function() {
 
 } ]).controller('MyCtrl2', [ '$scope', 'User', function($scope, User) {
 	$scope.user0 = User.get();
